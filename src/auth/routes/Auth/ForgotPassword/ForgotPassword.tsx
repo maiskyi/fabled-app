d3c34a1c2bf379fb573f7d3aa505cb90253bbd22
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-08-14 14:07:14 +0100

    feat: auth

--- src/auth/routes/Auth/ForgotPassword/ForgotPassword.tsx
+++ src/auth/routes/Auth/ForgotPassword/ForgotPassword.tsx
@@ -1,13 +1,19 @@
 import { memo } from 'react';
 
-import { Box, Content, Form, Header, Page, Text } from '@core/uikit';
+import { Box, Button, Content, Form, Header, Page, Text } from '@core/uikit';
 import { useTranslation } from '@core/localization';
 import { RoutePath } from '@bootstrap/constants';
+import { useRoute } from '@core/navigation';
 
 export const ForgotPassword = memo(function ForgotPassword() {
   const { t } = useTranslation();
+  const [, navigate] = useRoute();
 
   const title = t('pages.forgotPassword');
+
+  const handleOnContactSupport = () => {
+    navigate({ action: 'push', pathname: RoutePath.ContactUs });
+  };
 
   return (
     <Page>
@@ -29,6 +35,18 @@ export const ForgotPassword = memo(function ForgotPassword() {
               name="email"
               validation={{ email: true, required: true }}
             />
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={12}
+            padding={16}
+            paddingInline={20}
+          >
+            <Form.Submit>{t('actions.sendResetLink')}</Form.Submit>
+            <Button fill="outline" onClick={handleOnContactSupport}>
+              {t('actions.contactSupport')}
+            </Button>
           </Box>
         </Form>
       </Content>
