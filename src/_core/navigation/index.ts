84771d83a613aae6698ed603157254e1bf7e7435
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-09-11 00:11:54 +0100

    feat: app links

--- src/_core/navigation/index.ts
+++ src/_core/navigation/index.ts
@@ -1,13 +1,14 @@
 export { IonRouterOutlet as RouterOutlet } from '@ionic/react';
 export { Route } from 'react-router-dom';
-export { IonReactRouter as Router } from '@ionic/react-router';
 
+export * from './components/AppUrlListener';
+export * from './components/Link';
+export * from './components/LinkVoid';
+export * from './components/NavigateBack';
 export * from './components/NavigationProvider';
 export * from './components/ProtectedWithRedirect';
 export * from './components/Redirect';
-export * from './components/NavigateBack';
-export * from './components/Link';
-export * from './components/LinkVoid';
+export * from './components/Router';
 
 // Hooks
 export * from './hooks/useRoute';
