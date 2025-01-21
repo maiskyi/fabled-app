7baf830c9e6aae93829787f28585ca6cb1821ffa
Andrii Maiskyi <andrey@wearehelful.com>
2025-01-21 23:18:06 +0000

    feat: home. #138

--- src/_core/uikit/components/Header/HeaderTitle/HeaderTitle.tsx
+++ src/_core/uikit/components/Header/HeaderTitle/HeaderTitle.tsx
@@ -1,6 +1,9 @@
 import { ComponentProps, FC, PropsWithChildren } from 'react';
+import classNames from 'classnames';
 
 import { IonTitle } from '@ionic/react';
+
+import styles from './HeaderTitle.module.scss';
 
 type HeaderTitleProps = PropsWithChildren<{
   wrap?: boolean;
@@ -9,7 +12,12 @@ type HeaderTitleProps = PropsWithChildren<{
 
 export const HeaderTitle: FC<HeaderTitleProps> = ({ wrap, size, children }) => {
   return (
-    <IonTitle size={size}>
+    <IonTitle
+      className={classNames(styles.root, {
+        [styles.large]: size === 'large',
+      })}
+      size={size}
+    >
       {wrap ? <span className="ion-text-wrap">{children}</span> : children}
     </IonTitle>
   );
