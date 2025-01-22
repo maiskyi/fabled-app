398efcad40f95b58cd8977236b9ab54abeb9a40a
Andrii Maiskyi <andrey@wearehelful.com>
2025-01-22 00:17:12 +0000

    feat: home. #138

--- src/home/routes/Home/_partitions/FablesCreate/FablesCreate.tsx
+++ src/home/routes/Home/_partitions/FablesCreate/FablesCreate.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 
-import { Box, Card, Icon } from '@core/uikit';
+import { Card, Icon } from '@core/uikit';
 import { useTranslation } from '@core/localization';
 
 import { FablesCreateOnClickFn } from './FablesCreate.types';
@@ -15,16 +15,15 @@ export const FablesCreate = memo<FablesCreateProps>(function FablesCreate({
   const { t } = useTranslation();
 
   return (
-    <Box flex={0}>
-      <Card color="secondary" onClick={() => onClick({})}>
-        <Card.Header>
-          <Card.Subtitle>{t('intro.home')}</Card.Subtitle>
-          <Card.Title>{t('actions.createNewFable')}</Card.Title>
-        </Card.Header>
-        <Card.Footer>
-          <Icon name="arrow-forward-outline" size="large" />
-        </Card.Footer>
-      </Card>
-    </Box>
+    <Card color="horizontal" onClick={() => onClick({})}>
+      <Card.Content></Card.Content>
+      {/* <Card.Header>
+        <Card.Subtitle>{t('intro.home')}</Card.Subtitle>
+        <Card.Title>{t('actions.createNewFable')}</Card.Title>
+      </Card.Header>
+      <Card.Footer>
+        <Icon name="arrow-forward-outline" size="large" />
+      </Card.Footer> */}
+    </Card>
   );
 });
